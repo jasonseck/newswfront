2922d8b01b28911d95d945448b7deb583daeb367
Jason Seck <jasonseck@gmail.com>
2024-06-04 15:28:58 -0600

    wef

--- src/blocks/VideoHero.jsx
+++ src/blocks/VideoHero.jsx
@@ -5,7 +5,7 @@ export default function VideoHero({video,logo,text,textoverride}) {
         <div className="video_panel_wrap container">
             <div className="video_panel_inner">
                 <div className="video_panel_content_container">
-                    <video src="/amovie.mp4" autoPlay loop muted playsInline className="video_panel_video" poster="/facility_poster_blur.jpg"/>
+                    <video src="/bmovie.mp4" autoPlay loop muted playsInline className="video_panel_video" poster="/facility_poster_blur.jpg"/>
                     {textoverride ? (
                     <div className="video_text vault">HIGH ALTITUDE,<br/>HANDCRAFTED IN TAOS</div>
 
