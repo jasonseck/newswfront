d57d1040ff30676acace5c9c2e1e8b5d7343a8d4
Jason Seck <jasonseck@gmail.com>
2024-06-05 15:09:44 -0600

    wef

--- src/blocks/Signup.jsx
+++ src/blocks/Signup.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Image from 'next/image';
-
+import axios from 'axios';
 export default function Signup({preheading, heading, text}) {
+    const [email, setemail] = useState('jason');
+    const submitform = () => {
+        console.log('process')
+        console.log(process.env.NEXT_PUBLIC_BACKEND_URL)
+            fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/form-submissions`, {
+            method: 'POST',
+            mode:"no-cors",
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              form: "6660cf6e389fe10948604e39",
+              submissionData: {Email:email},
+            }),
+          }).then(res=>{
+            console.log('did it',res)
+          }).catch(error=>{
+            console.log('THERE WAS ERRER',error)
+          })
+          
+
+    }
     return (
         <div className="signup_wrap">
             <div className="signup_inner">
@@ -12,8 +34,17 @@ export default function Signup({preheading, heading, text}) {
                 </div>
                 <div className="signup_form">
                     <div className="signup_form_inner">
-                        <div className="signup_input"><input/></div>
-                        <div className="signup_button">submit</div>
+                        <div className="signup_input"><input
+                        value={email}
+                        onChange={(e)=>{
+                            setemail(e.target.value)
+                        }}
+                        /></div>
+                        <div className="signup_button"
+                            onClick={()=>{
+                               submitform();
+                            }}
+                        >submit</div>
                     </div>
                 </div>
                 <div className="signup_body_text text_secondary">{text}</div>
--- src/pages/SideNews.js
+++ src/pages/SideNews.js
@@ -3,17 +3,34 @@ import AppContext from '@/providers';
 import React, {useContext, useState} from 'react';
 import { CloseCircleOutline } from 'react-ionicons'
 import Link from 'next/link'
+import {CloseCircle} from 'react-ionicons';
 
-
+import { useRouter } from 'next/router';
 
 export default function SideNews({Component, pageProps}) {
+    const router = useRouter();
     const { sidenews, setsidenews } = useContext(AppContext);
     return sidenews ? (
         <div className="sidenews_wrapper">
             <div className="sidenews_inner">
-                <Link href="/deals">
+                <div className="sidenews_inner_wrap" onClick={()=>{
+                    router.push('/deals')
+                    setsidenews(false);
+
+                    }}>
                 <img src="/teaser@2x.png"/>
-                </Link>
+                <div className="sidenews_close"
+                    onClick={()=>{
+                        setsidenews(false);
+                    }}
+                >
+                    <CloseCircle
+                    color={'#303030'}
+                    width="35px"
+                    height="35px"
+                    />
+                </div>
+                </div>
             </div>
         </div>
     ) : null
