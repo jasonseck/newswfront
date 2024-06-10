5caba748704c2fc3eea61cb527ed4439c8b7dec1
Jason Seck <jasonseck@gmail.com>
2024-06-10 14:30:00 -0600

    wef

--- src/pages/rewardssignup2.js
+++ src/pages/rewardssignup2.js
@@ -3,18 +3,68 @@ import AppContext from '@/providers';
 import React, {useContext, useState} from 'react';
 import { CloseCircleOutline } from 'react-ionicons'
 
+const form = {id:"6660cf6e389fe10948604e39"}
 
 
 
 export default function RewardsSignup({Component, pageProps}) {
     const { agecheck } = useContext(AppContext);
     const { showsignup, setsignup } = useContext(AppContext);
-    return showsignup ? (
+    const [success, setSuccess] = useState(false)
+    const [email, setEmail] = useState();
+
+    const submitform = () => {
+        const temp = {
+          email:email
+      }
+      const dataToSend = Object.entries(temp).map(([name, value]) => ({
+        field: name,
+        value,
+      }))
+            fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/form-submissions`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              form: form.id,
+              submissionData: dataToSend
+            }),
+          }).then(res=>{
+            console.log('did it',res)
+            setSuccess(true)
+          }).catch(error=>{
+            console.log('THERE WAS ERRER',error)
+          })
+          
+    
+    }
+    
+
+    return showsignup ? success ? (
+        <div className="rewardssignup_wrapper">
+        <div className="rewardssignup_inner">
+            <div className="rs_box2">
+                <div className="rs_box_thanks">
+                    <div className="rs_box_thanks_title">THANKS!</div>
+                    <div className="rs_box_thanks_text">Check your email for a list of specials and a first time visit offer!</div>
+                    <div className="rs_box_thanks_close"
+                    onClick={()=>{
+                        setsignup(false)
+                    }}
+                    >CLOSE</div>
+                </div>  
+            </div>
+        </div>
+        </div>
+    ) : (
         <div className="rewardssignup_wrapper">
             <div className="rewardssignup_inner">
                 <div className="rs_box2">
+                <div className="rs_box_topline">DAILY SPECIALS</div>
 
                     <div className="rs_box_top">
+
                         <div className="rs_top_left">
                             <div className="rs_item">
                                 <div className="rs_item_title">MEDICAL <span className="rs_item_day">MONDAY</span></div>
@@ -60,9 +110,16 @@ export default function RewardsSignup({Component, pageProps}) {
                         </div>
 
                         <div className="rs_signup_row">
-                            <input/><a className="rs_button"
+                            <input
+                            value={email}
+                            onChange={(e)=>{
+                                setEmail(e.target.value)
+                            }}
+                            /><a className="rs_button"
                                                     onClick={()=>{
-                                                        setsignup(false);
+                                                        console.log(email)
+                                                        submitform();
+                                                        //setsignup(false);
                                                     }}
                             
                             >submit</a>
