5cee0d1e24eaf2dc02e425b92188c0b433dec7e7
Jason Seck <jasonseck@gmail.com>
2024-06-10 14:55:12 -0600

    wef

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -25,6 +25,7 @@ export default function App({ Component, pageProps }) {
       <div className="mobile_gate"> */}
           <Head>
             <title>Southwest Cannabis</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
           </Head>
           <AppProvider>
             <Inside Component={Component} pageProps={pageProps}/>
