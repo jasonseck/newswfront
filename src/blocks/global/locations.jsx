8f3b48110ca3d1ff8c96b2547c757d594a68f77f
Jason Seck <jasonseck@gmail.com>
2024-06-18 16:52:44 -0600

    wef

--- src/blocks/global/locations.jsx
+++ src/blocks/global/locations.jsx
@@ -5,9 +5,33 @@ import "react-sliding-pane/dist/react-sliding-pane.css";
 import { LocationOutline } from 'react-ionicons'
 export default function Locations(props) {
     const { location, setlocation, clearlocation, locationlist,listopen, togglelist, openlist, closelist } = useContext(AppContext);
-    useEffect(()=>{
-        return;
-    },[])
+
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+      });
+
+      useEffect(() => {
+        // only execute all the code below in client side
+        // Handler to call on window resize
+        function handleResize() {
+          // Set window width/height to state
+          setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+          });
+        }
+        
+        // Add event listener
+        window.addEventListener("resize", handleResize);
+         
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+        
+        // Remove event listener on cleanup
+        return () => window.removeEventListener("resize", handleResize);
+      }, []);
+
     return (
         <div className="location_selector_wrap" style={{width:props.width}}>
             <div class="location_selector_inside">
@@ -27,6 +51,7 @@ export default function Locations(props) {
                 title="Hey, it is optional pane title.  I can be React component too."
                 subtitle="Optional subtitle."
                 hideHeader={true}
+                width={windowSize.width > 767 ? "50%" : "100%"}
                 onRequestClose={() => {
                 // triggered on "<" on left top click or on outside click
                 closelist();
