3518e38e3693c5b9c6afd384985e849d0bbcf47a
Jason Seck <jasonseck@gmail.com>
2024-06-25 14:06:04 -0600

    wef

--- src/age/agecheck.js
+++ src/age/agecheck.js
@@ -1,9 +1,9 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
+import { setCookie, getCookie,deleteCookie } from 'cookies-next';
 import AppContext from '@/providers';
 const vid = require('@/img/Leaflylogo3.png')
 export default function AgeCheck() {
     const { agecheck, setagecheck, agedeclined, setagedeclined, setsignup } = useContext(AppContext);
-
     return (
         <div className="agecheck_wrapper">
             <div className="agecheck_inner">
@@ -15,13 +15,16 @@ export default function AgeCheck() {
                     <div className="agecheckquestion">Are you over the age of 21?</div>
 
                     ): (
+                        <>
                         <div className="agecheckquestion">Sorry, you must be over 21 to access this website.</div>
-
+                        {cs.get("swagegate") ? (<div>we have is</div>) : (<div>no cookie set</div>) }
+                        </>
                     )}
                     <div className="agecheckbuttonswrap">
                     <div className="agecheckbutton vault"
                         onClick={()=>{
                             console.log('clicked')
+                            setCookie('swagegate', true)
                             setagecheck();
                             setsignup(true);
 
