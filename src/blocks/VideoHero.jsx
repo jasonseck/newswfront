adadcbb89c89b41e2167f9e10ff55cd73f104d78
Jason Seck <jasonseck@gmail.com>
2024-08-02 10:39:54 -0600

    wef

--- src/blocks/VideoHero.jsx
+++ src/blocks/VideoHero.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-export default function VideoHero({video,logo,text,textoverride}) {
+export default function VideoHero({video,poster,logo,text,textoverride}) {
+    console.log(video)
     return (
         <div className="video_panel_wrap container">
             <div className="video_panel_inner">
                 <div className="video_panel_content_container">
-                    <video src="/bmovie.mp4" autoPlay loop muted playsInline className="video_panel_video" poster="/facility_poster_blur.jpg"/>
+                    <video src={video.url} autoPlay loop muted playsInline className="video_panel_video" poster={poster.url}/>
                     {textoverride ? (
                     <div className="video_text vault">HIGH ALTITUDE,<br/>HANDCRAFTED IN TAOS</div>
 
